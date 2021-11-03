6b3970fbcb181814d32c674406cc53b607682533
JacksonMHoke <jacksonmhoke@gmail.com>
2021-11-03 00:26:21 -0700

    Added base files with a few comments to plan out work

---
+++ core1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main () {
+    //sync with core 2
+
+    //calculate
+
+    //fetch data from core 2
+
+    //combined calculation
+
+    //finish timer
+}
---
+++ core2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main () {
+    //sync with core1
+
+    //calculate
+
+    //upload results to csv1
+}
