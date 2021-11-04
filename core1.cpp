b4a0c703a2ab317a3bd8cde4e032bbc45bc3fd1d
JacksonMHoke <jacksonmhoke@gmail.com>
2021-11-04 01:50:26 -0700

    Added syncing functionality through text file

--- core1.cpp
+++ core1.cpp
@@ -5,11 +5,22 @@
 
 using namespace std;
 
+static string syncFile = "sync.txt";
+
 int main () {
     //sync with core 2
+    ifstream fin;
+    string sync;
+    
+    while (sync!="ready") {
+        fin.open(syncFile);
+        fin >> sync;
+        fin.close();
+    }
 
+    cout << "FINISHED SYNCING\n";
     //calculate
-
+    
     //fetch data from core 2
 
     //combined calculation
--- core2.cpp
+++ core2.cpp
@@ -5,9 +5,12 @@
 
 using namespace std;
 
+static string syncFile = "sync.txt";
+
 int main () {
     //sync with core1
-
+    ofstream fout(syncFile);
+    fout << "ready";
     //calculate
 
     //upload results to csv1
