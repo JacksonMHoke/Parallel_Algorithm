61c9866364364604a024f4403e79a6e233ed86ff
JacksonMHoke <jacksonmhoke@gmail.com>
2021-11-04 18:32:29 -0700

    Finished parallel algorithm

--- core1.cpp
+++ core1.cpp
@@ -2,13 +2,24 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <unordered_map>
+#include <time.h>
+#include <math.h>
 
 using namespace std;
 
 static string syncFile = "sync.txt";
+static string dataFile = "data.txt";
+unsigned long long num = 12312123421342521200;
+
+
+// unsigned long long sumPermutations (vector<int>& digits, int currSum, int prevSum, int index) {
+
+// }
 
 int main () {
     //sync with core 2
+    clock_t timer = clock();
     ifstream fin;
     string sync;
     
@@ -17,13 +28,56 @@ int main () {
         fin >> sync;
         fin.close();
     }
+    timer = clock() - timer;
+    cout << "FINISHED SYNCING IN " << (float)timer/CLOCKS_PER_SEC << " SECONDS\n";
+    remove(syncFile.c_str());
 
-    cout << "FINISHED SYNCING\n";
-    //calculate
-    
-    //fetch data from core 2
+    //calculate all factors
+    timer = clock();
 
-    //combined calculation
+    unordered_map<int, int> factors;
+    while (num%2==0) {
+        cout << 2 << " ";
+        num/=2;
+        factors[2]++;
+    }
 
-    //finish timer
+    int sqrtNum = sqrt(num);
+    for (int i=3; i<sqrtNum; i+=2) {
+        if (num%i==0) {
+            cout << i << " ";
+            num/=i;
+            factors[i]++;
+        }
+    }
+
+    if (num>2) {
+        factors[num]++;
+        cout << num << " ";
+    }
+    cout << "\n";
+
+    //wait for data from core 2
+    sync="";
+    while (sync!="ready") {
+        fin.open(syncFile);
+        fin >> sync;
+        fin.close();
+    }
+
+    //fetch data from core 2/combined calculation
+    fin.open(dataFile);
+    int num;
+    string s;
+    while (fin >> num) {
+        cout << num << " ";
+        if (factors.find(num)!=factors.end() && factors[num]>0) {
+            s+=to_string(num)+" ";
+            factors[num]--;
+        }
+    }
+    cout << "\n" << s;
+
+    cout << "\nFINISHED COMBINED CALCULATION IN " << (float)(clock()-timer)/CLOCKS_PER_SEC << " SECONDS\n";
+    remove(syncFile.c_str());
 }
--- core2.cpp
+++ core2.cpp
@@ -2,16 +2,46 @@
 #include <fstream>
 #include <vector>
 #include <string>
+#include <math.h>
 
 using namespace std;
 
 static string syncFile = "sync.txt";
+static string dataFile = "data.txt";
+unsigned long long num = 1231231231231200000;
 
 int main () {
     //sync with core1
     ofstream fout(syncFile);
     fout << "ready";
+    fout.close();
     //calculate
+    vector<unsigned long long> data;
+    while (num%2==0) {
+        num/=2;
+        data.push_back(2);
+    }
+
+    unsigned long long sqrtNum = sqrt(num);
+    for (unsigned long long i=3; i<sqrtNum; i+=2) {
+        if (num%i==0) {
+            num/=i;
+            data.push_back(i);
+        }
+    }
+
+    if (num>2) data.push_back(num);
 
     //upload results to csv1
+    remove(dataFile.c_str());
+    fout.open(dataFile);
+    for (auto i : data) {
+        fout << i << " ";
+    }
+    fout.close();
+
+    //sync with core1
+    fout.open(syncFile);
+    fout << "ready";
+    fout.close();
 }
